0dcddc4eeed3d487ccf5a8043f1a4db93434798a
LuisBBandeira <luisfilipebentobandeira@gmail.com>
2024-02-02 21:58:04 +0000

    aula14

---
+++ aula14/app.js
@@ -0,0 +1,19 @@
+
+// var a = 9;
+// let b = 1;
+// const c = 'const c';
+
+
+// const func = () => {
+
+//     f = b + a
+//     console.log(f);
+// }
+
+
+// func()
+
+import Test2 from "./Test2.js";
+
+const test2 = new Test2();
+console.log(test2);
---
+++ aula14/app.py
@@ -0,0 +1,11 @@
+
+a = 'var a'
+b = 10
+c = 0
+
+def func():
+    f = b + c
+    print(f)
+
+
+func()
